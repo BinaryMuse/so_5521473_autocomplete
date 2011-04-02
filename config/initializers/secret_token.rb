4689bc2aca96a3df391fdd129acf5cc2329afe7f
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-02 14:32:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+So5521473Autocomplete::Application.config.secret_token = '5aa93bbe9c819792afecc9ee3ea95e11045adfaaf1c00a4317fa38be4cdcd92183f53f5b052e07fa7de277cb3306d139181241e2948bbedf49c8bdc6680e6599'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+So5521473Autocomplete::Application.config.session_store :cookie_store, :key => '_so_5521473_autocomplete_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# So5521473Autocomplete::Application.config.session_store :active_record_store
