c215d2374e267bb5aa84e5fbec6ff11ebcfb8e04
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-02 14:58:19 -0700

    Add Message model and set up associations

---
+++ app/models/message.rb
@@ -0,0 +1,3 @@
+class Message < ActiveRecord::Base
+  belongs_to :user, :foreign_key => "to"
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110402214959) do
+ActiveRecord::Schema.define(:version => 20110402215335) do
+
+  create_table "messages", :force => true do |t|
+    t.integer  "to"
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "first"
